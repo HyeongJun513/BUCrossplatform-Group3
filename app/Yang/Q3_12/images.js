cfddf08ff7b83a7277ce7054dca5b59b7639680b
youjin213 <yyoujin213@gmail.com>
2024-06-06 16:24:31 +0900

    Update images.js

--- app/Yang/Q3_12/images.js
+++ app/Yang/Q3_12/images.js
@@ -1,11 +1,11 @@
-import CheckBoxOutline from '../assets/icons/000.png';
-import CheckBox from '../assets/icons/000.png';
-import DeleteForever from '../assets/icons/000.png';
-import Edit from '../assets/icons/000.png';
+import hole1 from '..\images\hole1.png';
+import hole2 from '..\images\hole2.png';
+import hole3 from '..\images\hole3.png';
+import hole4 from '..\images\hole4.png';
 
 export const images = {
-    uncompleted: CheckBoxOutline,
-    completed: CheckBox,
-    delete: DeleteForever,
-    update: Edit,
+    uncompleted: hole1,
+    completed: hole2,
+    delete: hole3,
+    update: hole4,
 };
