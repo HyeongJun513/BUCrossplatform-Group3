54df4ea0cda6bab8c069d0500607d0b9d74c81dd
youjin213 <yyoujin213@gmail.com>
2024-06-14 23:44:31 +0900

    Update images.js

--- app/Yang/Q3_12/images.js
+++ app/Yang/Q3_12/images.js
@@ -2,10 +2,26 @@ import hole1 from './images/hole1.png';
 import hole2 from './images/hole2.png';
 import hole3 from './images/hole3.png';
 import hole4 from './images/hole4.png';
+import hole5 from './images/hole5.png';
+import hole6 from './images/hole6.png';
+import key1 from './images/key1.png';
+import key2 from './images/key2.png';
+import key3 from './images/key3.png';
+import key4 from './images/key4.png';
+import key5 from './images/key5.png';
+import key6 from './images/key6.png';
 
 export const images = {
-    uncompleted: hole1,
-    completed: hole2,
-    delete: hole3,
-    update: hole4,
+  hole1,
+  hole2,
+  hole3,
+  hole4,
+  hole5,
+  hole6,
+  key1,
+  key2,
+  key3,
+  key4,
+  key5,
+  key6,
 };
